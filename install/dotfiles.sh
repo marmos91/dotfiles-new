b3548c8912461d28101237a7d8db239ba627f50a
Marco Moschettini <m.marmos@gmail.com>
2023-05-31 10:12:50 +0200

    add tmux powerline

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -8,3 +8,4 @@ ln -sfv "$DOTFILES_DIR/dotfiles/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dotfiles/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dotfiles/.alias" ~
 ln -sfv "$DOTFILES_DIR/dotfiles/.env" ~
+ln -sfv "$DOTFILES_DIR/dotfiles/.tmux-powerlinerc" ~
