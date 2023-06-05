491a60a8c54a389dce6db19db6e14892e5315f57
Marco Moschettini <m.marmos@gmail.com>
2023-06-05 12:40:09 +0200

    improve mappings

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -1,32 +1,32 @@
 local M = {}
 
 M.general = {
-  n = {
-    -- Tmux motions
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+	n = {
+		-- Tmux motions
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
 
-    -- lazygit stuff
-    ["<leader>gg"] = { "<cmd> LazyGit<CR> "},
+		-- lazygit stuff
+		["<leader>gg"] = { "<cmd> LazyGit<CR> " },
 
-    -- Keep clipboard register while pasting
-    ["<leader>p"] = { "\"_dP" },
+		-- Keep clipboard register while pasting
+		["<leader>p"] = { '"_dP' },
 
-    -- move by page while centering cursor
-    ["<C-d>"] = { "<C-d>zz", "go down half page" },
-    ["<C-u>"] = { "<C-u>zz", "go up half page" },
-
-     -- move a line up/down
-    ["<A-j>"] = { "<Esc>:m .+1<CR>==gi", "move line down" },
-    ["<A-k>"] = { "<Esc>:m .-2<CR>==gi", "move line up" },
-  },
-  i = {
-     -- move a line up/down
-    ["<A-j>"] = { "<Esc>:m .+1<CR>==gi", "  imove line down" },
-    ["<A-k>"] = { "<Esc>:m .-2<CR>==gi", "  imove line up" },
-  }
+		-- move by page while centering cursor
+		["<C-d>"] = { "<C-d>zz", "go down half page" },
+		["<C-u>"] = { "<C-u>zz", "go up half page" },
+	},
+	i = {
+		-- move a line up/down
+		["<A-j>"] = { "<Esc>:m .+1<CR>==gi", "move line down" },
+		["<A-k>"] = { "<Esc>:m .-2<CR>==gi", "move line up" },
+	},
+	v = {
+		["<S-j>"] = { ":m '>+1<CR>gv=gv" },
+		["<S-k>"] = { ":m '<-2<CR>gv=gv" },
+	},
 }
 
 return M
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -2,53 +2,53 @@ local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "ThePrimeagen/vim-be-good",
-    lazy = false,
-  },
-  {
-    "derektata/lorem.nvim",
-    lazy = false,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    lazy = false,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-  }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+		lazy = false,
+	},
+	{
+		"derektata/lorem.nvim",
+		lazy = false,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		lazy = false,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+	},
 }
 
 return plugins
