829a68406ad2f74d1c50c534da3104248b4ce988
Marco Moschettini <m.marmos@gmail.com>
2023-06-19 16:07:34 +0200

    fix: remove broken binding

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls", "gopls", "bashls" }
+local servers = { "html", "cssls", "gopls", "bashls", "tsserver" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
@@ -12,10 +12,10 @@ for _, lsp in ipairs(servers) do
 	})
 end
 
-lspconfig.tsserver.setup({
-	on_attach = function(client)
-		-- Temporary disabling this due to a bug with tsserver indentation
-		-- disable formatting for tsserver
-		client.server_capabilities.documentFormattingProvider = false
-	end,
-})
+-- lspconfig.tsserver.setup({
+-- 	on_attach = function(client)
+-- 		-- Temporary disabling this due to a bug with tsserver indentation
+-- 		-- disable formatting for tsserver
+-- 		client.server_capabilities.documentFormattingProvider = false
+-- 	end,
+-- })
