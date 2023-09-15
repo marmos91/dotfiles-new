43b6bf2e6c6bcd103b104ad44819c9f6fec68040
Marco Moschettini <m.marmos@gmail.com>
2023-09-15 17:22:45 +0200

    feat: upgrade alacritty

--- macos/cli-install.sh
+++ macos/cli-install.sh
@@ -8,15 +8,15 @@ fi
 set -eo pipefail
 
 if ! directory-exists /Applications/Alacritty.app; then
-	wget https://github.com/alacritty/alacritty/releases/download/v0.12.1/Alacritty-v0.12.1.dmg
+	wget https://github.com/alacritty/alacritty/releases/download/v0.12.2/Alacritty-v0.12.2.dmg
 
-	hdiutil mount Alacritty-v0.12.1.dmg
+	hdiutil mount Alacritty-v0.12.2.dmg
 
 	cp -R "/Volumes/Alacritty/Alacritty.app" /Applications
 
 	hdiutil unmount "/Volumes/Alacritty/"
 
-	rm Alacritty-v0.12.1.dmg
+	rm Alacritty-v0.12.2.dmg
 
 	echo "Alacritty installed"
 
