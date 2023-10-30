b20f2928cf46901fb97647bc27e48b2e925f1d6f
Marco Moschettini <m.marmos@gmail.com>
2023-10-30 14:30:09 +0100

    feat: set tilt filetype

--- config/nvim/lua/custom/autocmds.lua
+++ config/nvim/lua/custom/autocmds.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	pattern = "*.mdx",
 	command = "setfiletype markdown",
 })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = "*.tilt",
+	command = "setfiletype bzl",
+})
