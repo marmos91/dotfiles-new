34356fd70f4241350408994661f14ee9af84cb81
Marco Moschettini <marmos91@Marcos-MacBook-Pro.local>
2024-05-03 09:17:45 +0200

    chore: add brew to path

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,23 +1,26 @@
 
 # System stuff
-fish_add_path "$HOME/bin"
+fish_add_path ~/bin
 fish_add_path /usr/local/bin
 fish_add_path /usr/local/sbin
-fish_add_path "$HOME/.local/bin"
+fish_add_path ~/.local/bin
 
 # Custom commands
-fish_add_path "$DOTFILES_DIR/bin"
+fish_add_path $DOTFILES_DIR/bin
 
 # Python stuff
-fish_add_path "$HOME/.poetry"
+fish_add_path ~/.poetry
 
 # Go stuff
 fish_add_path /usr/local/go/bin
-fish_add_path "$GOPATH/bin"
+fish_add_path $GOPATH/bin
 
 # Rust stuff
-fish_add_path "$HOME/.cargo/bin"
-fish_add_path "$HOME/.rustup"
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.rustup
 
 # Lua stuff
-fish_add_path "$HOME/.luarocks/bin"
+fish_add_path ~/.luarocks/bin
+
+# Brew stuff
+fish_add_path /opt/homebrew/bin
