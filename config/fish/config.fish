b841876885c4d4d009e44c315092fc0f2ef29d62
Marco Moschettini <m.marmos@gmail.com>
2024-05-08 14:27:51 +0200

    fix: remove fish sponge

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,10 @@
 # Tmux
 
 set -e fish_tmux_default_session_name mac
+
+# pnpm
+set -gx PNPM_HOME "/Users/marmos91/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
