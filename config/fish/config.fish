5946559ff753accc7295fe332bbe114228b34e2e
Marco Moschettini <m.marmos@gmail.com>
2024-05-16 15:12:49 +0200

    chore: use node 18 only if nvm is installed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,9 @@
 
 set -e fish_tmux_default_session_name mac
 
-nvm use 18
+if type -q nvm; and not type -q node
+    nvm use 18 >/dev/null
+end
 
 # pnpm
 set -gx PNPM_HOME /Users/marmos91/Library/pnpm
