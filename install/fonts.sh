99a95de9048670feb13ac45e6e139a65c43b118f
Marco Moschettini <m.marmos@gmail.com>
2024-06-27 09:49:45 +0200

    chore: improve wezterm conf

--- install/fonts.sh
+++ install/fonts.sh
@@ -5,12 +5,13 @@ if ! is-macos -o ! is-executable brew; then
 	return
 fi
 
-brew tap homebrew/cask-fonts
+brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
 
 fonts=(
 	font-fira-code-nerd-font
 	font-jetbrains-mono-nerd-font
 	font-meslo-lg-nerd-font
+	font-sf-mono-nerd-font-ligaturized
 )
 
 brew install --cask "${fonts[@]}"
